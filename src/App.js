33d184619f22015b05bea36c434c45d26308e02b
loicgeek <loic.ngou@1menu.me>
2020-03-26 15:40:28 +0100

    added route from section item to section details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 
+import { Route, Switch } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>hATS PAGE</h1>
+  </div>
+);
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
